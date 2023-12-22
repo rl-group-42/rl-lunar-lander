3d3429f4ecd5193fce5a64800dd35404967b66ca
Saleh Razian <sr2184@bath.ac.uk>
2023-12-22 00:04:09 +0000

    with test params
    For testing

--- ppo_code_2.py
+++ ppo_code_2.py
@@ -4,10 +4,22 @@ import torch.nn as nn
 import torch.optim as optim
 import gymnasium as gym
 import torch.distributions as dist
-import torch.nn.functional as F
 from torch import cuda
 from torch.backends import mps
 from collections import deque
+import csv
+
+number_exp = "network_1_128_c"
+base_name = "C:/Users/HP/Documents/1 FILE 7658 Codex/University/RL/CODE/Data/"
+file_name_csv_optimal_average = base_name + "ppo_test_csv_oprimal_average_" + str(number_exp) + ".csv"
+file_name_csv_optimal =  base_name + "ppo_test_csv_oprimal_" + str(number_exp) + ".csv"
+file_name_txt =  base_name + "ppo_test_txt_" + str(number_exp) + ".txt"
+file_name_csv_training_episodes = base_name + "ppo_test_csv_training_episodes_" + str(number_exp) + ".csv"
+
+def write_data(file_path, *data):
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([*data])
 
 def get_device() -> str:
     """ Get Device Name """
@@ -47,7 +59,7 @@ class AgentMemory:
     
 
 class Network(nn.Module):
-    def __init__(self, input_dim, output_dim, output_activations=tuple(), hidden_dims=(64, 32), hidden_activations=None, learning_rate=0.01):
+    def __init__(self, input_dim, output_dim, output_activations=tuple(), hidden_dims=(128,), hidden_activations=None, learning_rate=0.01):
         super(Network, self).__init__()
 
         self._input_dim = input_dim
@@ -90,6 +102,7 @@ class Agent:
     def __init__(self, env, continuity=True, optimal=False, gamma=0.9, lambda_gae=0.95, 
                  value_ratio_loss=0.001, entropy_loss_ratio=0.001,
                  learning_rates=(0.01, 0.2), policy_clip=0.2, training_iterations=1000, 
+                 optimal_run_episodes=30, render_episode=1, measure_after_iterations=10, measurement=True,
                  episodes=20, epoches=40, minibatch_size=256, memory_order=None):
 
         # Environment 
@@ -106,6 +119,13 @@ class Agent:
         self._episodes = episodes
         self._epoches = epoches
         self._minibatch_size = minibatch_size
+        self._updates_number = 0
+
+        # Optimal Run
+        self._optimal_run_episodes = optimal_run_episodes
+        self._render_episode = render_episode
+        self._measure_after_iterations = measure_after_iterations
+        self._measurement = measurement
 
         # Memory
         self._memory_order = memory_order
@@ -138,6 +158,9 @@ class Agent:
 
         self._actor_network = Network(self._input_dim, self._output_dim, activations, learning_rate=learning_rates[0]).to(self._device)
         self._critic_network = Network(self._input_dim, 1, learning_rate=learning_rates[1]).to(self._device)
+        self._actor_network.train()
+        self._critic_network.train()
+
 
         print(self._actor_network, self._critic_network)
 
@@ -148,12 +171,13 @@ class Agent:
         return T.from_numpy(nparray).float().to(self._device).requires_grad_(grad)
 
     def train_run(self):
-
+        seed = 0
         latest_scores = deque(maxlen=100)
         total_reward = 0
+        t=0
+        terminal = True
         # Train Cycles
-        for iteration in range(self._training_iterations):
-            terminal = True
+        for iteration in range(1, self._training_iterations):
             trajectory = []
 
             # Collecting Trajectories D_k = {tau_i}
@@ -161,11 +185,13 @@ class Agent:
                 
                 if terminal:
                     # Setup
-                    current_state = self._env.reset()[0]
+                    seed += 1
+                    current_state = self._env.reset(seed=seed)[0]
                     latest_scores.append(total_reward)
+                    write_data(file_name_csv_training_episodes, iteration * episode, episode - t, total_reward)
+                    t = episode
                     total_reward = 0
                     terminal = False
-
 
                 # Environment Interaction
                 action, log_probability = self._get_action(current_state)
@@ -183,22 +209,76 @@ class Agent:
 
             # Forget
             self._memory.new_memory()
+
+            print(f"Iteration:{iteration}, avg_total_reward:{np.mean(latest_scores)}")
             
-            if iteration % 1 == 0:
-                print(f"Iteration:{iteration}, total_reward:{np.mean(latest_scores)}")
+            if iteration % self._measure_after_iterations == 0 and self._measurement:
+                # Measure improvement
+                self.optimal_run(seed)
+
+    def optimal_run(self, seed=0):
+        print("\nTest\n")
+        optimal_seed = seed
+        self._optimal = True
+        latest_scores = []
+        rendered = False
+        for episode in range(self._optimal_run_episodes):
+            optimal_seed += 1
+            render_condition = episode < self._render_episode
+            if render_condition:
+                env = gym.make("LunarLander-v2", 
+                                continuous=self._continuity,
+                                #gravity=-10.0, 
+                                #enable_wind=False, 
+                                #wind_power=15.0, 
+                                #turbulence_power=1.5, 
+                                render_mode="human"
+                                )
+                rendered = True
+            else:
+                if rendered:
+                    env.close()
+                env = self._env
+                rendered = False
+            current_state = env.reset(seed=optimal_seed)[0]
+            episode_score = 0
+            terminal = False
+            render_condition = episode % self._render_episode == 0
+
+            while not terminal:
+                action, _ = agent._get_action(current_state)
+                # Take action and observe experience
+                new_state, reward, terminal, truncated, info = env.step(action)
+                terminal = terminal or truncated
+                current_state = new_state
+                episode_score += reward
+
+            latest_scores.append(episode_score)
+            write_data(file_name_csv_optimal, episode_score)
+
+            print(f"Episode:{episode}, avg_total_reward:{np.mean(latest_scores)}")
+        self._optimal = False
+        if rendered:
+            env.close()
+
+        write_data(file_name_csv_optimal_average, self._updates_number, float(np.mean(latest_scores)))
+        print("\nEnd Test\n")
+
 
     def _get_action(self, state):
         # Convert to Tensor
         state_tensor = self._get_tensor(np.array([state]), False)
 
         # Network Prediction of Action Probabilities pi_Theta_k(a_t|s_t)
+        self._actor_network.eval()
         with T.no_grad():
             action_probabilities = self._actor_network(state_tensor)
+        self._actor_network.train()
 
         if self._optimal:
             # Optimal action is of max probability (mu for continuous space)
             action = action_probabilities[0] if self._continuity else T.argmax(action_probabilities[0], dim=-1)
-            log_probability = T.constant([1.], dtype=T.float32)
+            log_probability = T.tensor([1.], dtype=T.float32)
         else:
             # Action based on probability distribution (mu and sigma used in continuous space)
             distribution = dist.Normal(action_probabilities[0], action_probabilities[1]) if self._continuity else dist.Categorical(action_probabilities)
@@ -240,6 +320,7 @@ class Agent:
             advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-6)
 
             sample_size = len(states)
+            space_permutation = np.random.permutation(np.arange(sample_size))
             index = 0
 
             while index < sample_size:
@@ -249,7 +330,7 @@ class Agent:
                     end_index = sample_size - 1
                 batch = range(index, end_index)
 
-                permute = np.random.permutation(np.array(batch))
+                permute = space_permutation[batch]
 
                 state = self._get_tensor(states[permute])
                 action = self._get_tensor(actions[permute])
@@ -261,15 +342,17 @@ class Agent:
                 action_probabilities = self._actor_network(state)
 
                 critic_loss = nn.MSELoss()(value, reward_return)
-                loss = self._loss_function(action_probabilities, action, advantage, log_probability, critic_loss)
+                loss = self._loss_function(action_probabilities, action, advantage, log_probability, critic_loss.detach())
+
+                self._critic_network.optimizer.zero_grad()
+                critic_loss.backward()
+                self._critic_network.optimizer.step()
 
                 self._actor_network.optimizer.zero_grad()
-                self._critic_network.optimizer.zero_grad()
-                
                 loss.backward()
-
                 self._actor_network.optimizer.step()
-                self._critic_network.optimizer.step()
+                
+                self._updates_number += 1
 
                 index += self._minibatch_size
 
@@ -311,9 +394,10 @@ class Agent:
 
         # c_1 L_t^VF(Theta) = c_1 MSE = c_1 (V_Theta(s_t) - V_t^target)^2
         # Negative sign is not used since MSE is being used
+        # Might need to be removed as a separate network is doing this
         critic_loss = self._value_loss_ratio * critic_loss
 
-        # c_2 S[pi_Theta](s_t) = c_2 Entropy Bonus
+        # c_2 S[pi_Theta](s_t) = c_2 Entropy Bonus To ensure sufficient exploration
         if self._continuity:
             sigma = predicted_action_probabilities[0]
             variance = sigma.pow(2)
@@ -324,18 +408,26 @@ class Agent:
         loss = actor_loss + critic_loss
         loss = loss + entropy_loss
         return loss
-    
+
+
 training_iterations = 10000
-episodes = 1000
-epoches = 32
-batch_size = 256
+episodes = 1024 # Steps per gradient
+epoches = 32 # epoches of complete batch updates
+batch_size = 256 # Batches of the step per decent
+# 32 * (1024 / 256) = gradient steps = 128
+
+optimal_run_episodes = 10
+render_episode = -1
+measure_after_iterations = 1
+measurement = True
+
 memory_order = ("state", "action", "log_probability", "terminal", "reward", "new_state")
-gamma = 0.99
-lambda_gae = 0.97
-value_ratio_loss = 0.01
-entropy_loss_ratio = 0.01
-learning_rates = [0.0003, 0.001]
-policy_clip = 0.15
+gamma = 0.999 # good
+lambda_gae = 0.9 # Decrease
+value_ratio_loss = 0. # Might remain 0
+entropy_loss_ratio = 0.005 # check
+learning_rates = [0.00025, 0.0005] # If learning slowly increase
+policy_clip = 0.15 # if updates are high ddecrease
 continuity = True
 optimal = False
 env = gym.make("LunarLander-v2", 
@@ -344,15 +436,49 @@ env = gym.make("LunarLander-v2",
          #enable_wind=False, 
          #wind_power=15.0, 
          #turbulence_power=1.5, 
-         #render_mode="human"
          )
 
-agent = Agent(env, 
+write_data(file_name_txt, """
+           training_iterations = 10000
+            episodes = 1024 # Steps per gradient
+            epoches = 32 # epoches of complete batch updates
+            batch_size = 256 # Batches of the step per decent
+            # 32 * (1024 / 256) = gradient steps = 128
+
+            optimal_run_episodes = 10
+            render_episode = -1
+            measure_after_iterations = 1
+            measurement = True
+
+            memory_order = ("state", "action", "log_probability", "terminal", "reward", "new_state")
+            gamma = 0.999 # good
+            lambda_gae = 0.9 # Decrease
+            value_ratio_loss = 0. # Might remain 0
+            entropy_loss_ratio = 0.01 # If exploring too much decrease
+            learning_rates = [0.00025, 0.0005] # If learning slowly increase
+            policy_clip = 0.15 # if updates are high ddecrease
+            continuity = True
+            optimal = False
+            env = gym.make("LunarLander-v2", 
+                    continuous=continuity
+                    #gravity=-10.0, 
+                    #enable_wind=False, 
+                    #wind_power=15.0, 
+                    #turbulence_power=1.5, 
+                    )
+           """)
+
+
+agent = Agent(env,
               continuity=continuity, 
               optimal=optimal, 
               training_iterations=training_iterations,
               episodes=episodes,
               epoches=epoches,
+              optimal_run_episodes=optimal_run_episodes, 
+              render_episode=render_episode, 
+              measure_after_iterations=measure_after_iterations, 
+              measurement=measurement,
               minibatch_size=batch_size,
               gamma=gamma,
               lambda_gae=lambda_gae,
